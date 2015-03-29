18e6af28be664ddcc47b2690178a8fbbb94e27a5
edmundophie <edmund.ophie@yahoo.com>
2015-03-30 05:07:03 +0700

    Inital commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,14 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller {
+
+	public function index() {
+		return view("pages.index");
+	}
+
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,222 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Citizen Report</title>
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <!--[if lt IE 9]>
+     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!--  webfonts  -->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+<!-- // webfonts  -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- start plugins -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="header_bg"><!-- start header -->
+	<div class="container">
+		<div class="row header">
+		<nav class="navbar" role="navigation">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="" class="img-responsive"/> </a>
+		    </div>
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="menu nav navbar-nav ">
+		        <li class="active"><a href="index.html">home</a></li>
+		        <li><a href="feature.html">daftar pengaduan</a></li>
+		        <li><a href="blog.html">statistik</a></li>
+		        <li><a href="about.html">buat pengaduan</a></li>
+		        <li id="loggedin-menu">
+		        	welcome, <a href="contact.html">edmund</a> 
+	        	</li>
+	        	<li id="notification-menu">
+		        	<a href="#" class="glyphicon glyphicon-bell"></a>
+	        	</li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+		</div>
+		<div class="row slider text-center">
+			<div class="col-md-8">
+					<div class="col-md-10 slider_text">
+						<h2>Punya Keluhan Untuk Pemkot Bandung ?</h2>
+						<h3>Disini Tempatnya</h3>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="slider_img">
+						<img src="images/pic1.png" alt="" class="img-responsive"/>
+					</div>
+				</div>
+			</div>
+	</div>
+</div>
+<div class="main"><!-- start main -->
+<div class="container main">
+	<div class="row grids_of_3">
+				<div class="col-md-4 grid1_of_3">
+					  <h2>Statistik</h2>
+					  <img src="images/icon1.png" class="img-responsive"/>
+					  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				     <div class="rd_more1">
+						<a href="single-page.html"><button class="btn_style">lebih lanjut</button></a>
+					</div>					
+				</div>
+				<div class="col-md-4 grid1_of_3">
+					<h2>Social Media</h2>
+					  <img src="images/icon2.png" class="img-responsive"/>
+					  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				      <div class="rd_more1">
+						<a href="single-page.html"><button class="btn_style">lebih lanjut</button></a>
+					</div>					
+				</div>
+				<div class="col-md-4 grid1_of_3">
+					<h2>Trending</h2>
+					  <img src="images/icon3.png" class="img-responsive"/>
+					  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				     <div class="rd_more1">
+						<a href="single-page.html"><button class="btn_style">lebih lanjut</button></a>
+					</div>	
+				</div>
+			    <div class="clearfix"></div>
+	</div>
+	<div class="row grids_btm top">
+		<div class="col-md-6">
+			<div class="grid_list">
+				<div class="images_1_of_1">
+					<p>27</p>
+				</div>
+				<div class="grid_1_of_1">
+					  	<h3>Pembuatan paspor di kantor imigrasi lamban</h3>
+						<p>Cum sociis natoque penatibus et dis parturient montes, nascetur ridiculus mus. Cras mattis purus sit amet...<span class="hide">sed do eiusmod tempor incididunt magna aliqua...</span> </p>	   
+	 			</div>
+	 			 <div class="clearfix"></div>
+			</div>
+			</div>
+			<div class="col-md-6">
+				<div class="grid_list">
+				<div class="images_1_of_1">
+					<p>24</p>
+				</div>
+					<div class="grid_1_of_1">
+					  	<h3>Pembuatan E-KTP tak kunjung selesai di kantor camat Coblong</h3>
+						<p>Nullam id dolor id nibh ultricies vehicula ut id elit. Vestibulum id ligula porta felis euismod semper...<span class="hide">sed do eiusmod tempor incididunt magna aliqua.</span> </p>   
+	 				</div>
+				</div>
+				 <div class="clearfix"></div>
+			</div>
+	</div>
+	<div class="row grids_btm top">
+		<div class="col-md-6">
+			<div class="grid_list">
+				<div class="images_1_of_1">
+					<p>19</p>
+				</div>
+				<div class="grid_1_of_1">
+					  	<h3>Banyak calo saat mengurus pajak reklame di kantor pajak</h3>
+						<p>Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue...<span class="hide">sed do eiusmod tempor incididunt magna aliqua.</span> </p>	   
+	 			</div>
+	 			 <div class="clearfix"></div>
+			</div>
+			</div>
+			<div class="col-md-6">
+				<div class="grid_list">
+				<div class="images_1_of_1">
+					<p>15</p>
+				</div>
+					<div class="grid_1_of_1">
+					  	<h3>Parkir liar kendaraan di sepanjang jalan dago</h3>
+						<p>Morbi risus, porta ac consectetur ac, vestibulum at eros. Nullam quis risus eget urna mollis ornare vel eu leo...<span class="hide">sed do eiusmod tempor incididunt magna aliqua...</span> </p>   
+	 				</div>
+				</div>
+				 <div class="clearfix"></div>
+			</div>
+	</div>
+</div>
+</div>
+<div class="footer_bg"><!-- start footre -->
+	<div class="container">
+		<div class="row  footer">
+			<div class="col-md-3 span1_of_4">
+				<h4>tentang kami</h4>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry .....</p>
+				<h5>Address</h5>
+				<p class="top">500 Lorem Ipsum Dolor Sit,</p>
+				<p>22-56-2-9 Sit Amet,</p>
+				<p>USA</p>
+				<p>Phone:(00) 222 666 444</p>
+				<p>Fax: (000) 000 00 00 0</p>
+			</div>
+			<div class="col-md-3 span1_of_4">
+				<h4>Aduan terbaru</h4>
+				<span><a href="#"> Fusce scelerisque massa vitae </a></span>
+				<p>25 april 2013</p>
+				<span><a href="#">Pellentesque bibendum ante </a></span>
+				<p>15 march 2013</p>
+				<span><a href="#">Maecenas quis ipsum sed magna </a></span>
+				<p>25 april 2013</p>
+			</div>
+			<div class="col-md-3 span1_of_4">
+				<h4>respon terbaru</h4>
+				<span><a href="#">It is a long established fact that a reader will looking layout.</a></span>
+				<span><a href="#">There are many variations of passages of Lorem Ipsum available words.</a></span>
+				<span><a href="#">It is a long established fact that a reader will looking layout.</a></span>
+			</div>
+			<div class="col-md-3 span1_of_4">
+				<h4>statistik aduan</h4>
+
+				<h5>100 pengaduan sudah ditindaklanjuti</h5>
+				<h5>72 pengaduan pending</h5>
+				<h5>32 pengaduan sedang dikerjakan</h5>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</div>
+<div class="footer_btm"><!-- start footer_btm -->
+	<div class="container">
+		<div class="row  footer1">
+			<div class="col-md-5">
+				<div class="soc_icons">
+					<ul class="list-unstyled">
+						<li><a class="icon1" href="#"></a></li>
+						<li><a class="icon2" href="#"></a></li>
+						<li><a class="icon3" href="#"></a></li>
+						<li><a class="icon4" href="#"></a></li>
+						<li><a class="icon5" href="#"></a></li>
+						<div class="clearfix"></div>
+					</ul>	
+				</div>
+			</div>
+			<div class="col-md-7 copy">
+				<p class="link text-right"><span>&#169; All rights reserved | Design by&nbsp;<a href="http://w3layouts.com/"> W3Layouts</a></span></p>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
