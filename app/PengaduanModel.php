38df2898134c0273c56de4f22042306131413087
edmundophie <edmund.ophie@yahoo.com>
2015-04-06 11:45:11 +0700

    status

--- app/PengaduanModel.php
+++ app/PengaduanModel.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
 class PengaduanModel extends Model{
-    private $namaKategori;
     protected $table = "pengaduan";
 
 }
