7669f7479b303dab80833ab776dbf9ac94952b92
edmundophie <edmund.ophie@yahoo.com>
2015-05-03 07:01:49 +0700

    asd

--- app/SKPDModel.php
+++ app/SKPDModel.php
@@ -8,7 +8,16 @@ class SKPDModel extends Model {
 	protected $table = "ppl_dukcapil_ktp";
 	protected $appends = array('masuk');
 	
-	public function getMasukAttribute($value) {
-		return "MASUK";
+	public function getNamaKategoriAttribute() {
+		$id_kategori = DB::table('ppl_citizenreport_penanggungjawab')->where('id_skpd', $this->attributes['id'])->first()->id_kategori;
+		$nama_kategori = KategoriModel::where('id', $id_kategori)->first()['nama'];
+
+		return $nama_kategori;
+	}
+
+	public function getIdKategoriAttribute($value) {
+		$id_kategori = DB::table('ppl_citizenreport_penanggungjawab')->where('id_skpd', $this->attributes['id'])->first()->id_kategori;
+
+		return $id_kategori;
 	}
 }
