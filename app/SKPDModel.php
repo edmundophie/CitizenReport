859980a5aed1ca3950f43f87c663a649ef34cdde
edmundophie <edmund.ophie@yahoo.com>
2015-05-03 07:40:31 +0700

    asd

--- app/SKPDModel.php
+++ app/SKPDModel.php
@@ -7,17 +7,16 @@ use App\KategoriModel;
 class SKPDModel extends Model {
 	protected $table = "ppl_dukcapil_ktp";
 	protected $appends = array('id_kategori', 'kategori');
-	
-	public function getKategoriAttribute() {
-		$id_kategori = DB::table('ppl_citizenreport_penanggungjawab')->where('id_skpd', $this->attributes['id'])->first()->id_kategori;
-		$nama_kategori = KategoriModel::where('id', $id_kategori)->first()['nama'];
-
-		return $nama_kategori;
-	}
 
 	public function getIdKategoriAttribute($value) {
 		$id_kategori = DB::table('ppl_citizenreport_penanggungjawab')->where('id_skpd', $this->attributes['id'])->first()->id_kategori;
 
 		return $id_kategori;
+	}
+
+	public function getKategoriAttribute() {
+		$id_kategori = DB::table('ppl_citizenreport_penanggungjawab')->where('id_skpd', $this->attributes['id'])->first()->id_kategori;
+		dd($id_kategori);
+		return KategoriModel::where('id', $id_kategori)->first()['nama'];
 	}
 }
