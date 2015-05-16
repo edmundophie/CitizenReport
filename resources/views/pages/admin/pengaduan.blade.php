b1633a1dacd0b0f91f94126c3e979a19a8d95b27
Fahmi Dumadi <fah.midum.adi@gmail.com>
2015-05-16 20:52:01 +0700

    tes

--- resources/views/pages/admin/pengaduan.blade.php
+++ resources/views/pages/admin/pengaduan.blade.php
@@ -105,7 +105,7 @@
 				</tr>
 				@foreach($listIMB as $IMB)
 				<tr>
-					<td>($listIMB['pemeganghak'])</td>
+					<td>$listIMB['pemeganghak']</td>
 					<td>$listIMB['updated_at']</td>
 					<td>$listIMB['lokasi']</td>
 					<td>$listIMB['kategori']</td>
