5157ecd1f5f6eada7c19e6153d1183beddc1a56d
Fahmi Dumadi <fah.midum.adi@gmail.com>
2015-05-16 18:08:58 +0700

    coba

--- app/Verifier.php
+++ app/Verifier.php
@@ -1,6 +1,7 @@
-<?php
+<?php 
 
 namespace App;
+
 use Illuminate\Database\Eloquent\Model;
 use DB;
 
@@ -9,7 +10,7 @@ class Verifier extends Model {
 	//
 	protected $table = "ppl_imb_permohonans";
 	
-	public function getlistIMB($alamat){
+	public static function getlistIMB($alamat){
 		$list = DB::table("ppl_imb_permohonans")
 					->join('ppl_imb_bangunans', 'ppl_imb_permohonans.bangunan_nomor', '=', 'ppl_imb_bangunans.nomor')
 			        ->where('lokasi','LIKE', '%'.$alamat.'%')
