b8a436f326c98eebdfa177db3508368a55d5e7d1
edmundophie <edmund.ophie@yahoo.com>
2015-05-17 09:16:38 +0700

    Fix some role issue

--- resources/views/pages/master.blade.php
+++ resources/views/pages/master.blade.php
@@ -53,7 +53,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		        <li @yield('home_active')><a href="{{ URL::to('index') }}"><span class="glyphicon glyphicon-home"></span> home</a></li>
 		        <li @yield('daftar_pengaduan_active')><a href="{{ URL::to('daftar-pengaduan/default') }}"><span class="glyphicon glyphicon-list"></span> daftar pengaduan</a></li>
 		        <li @yield('statistik_active')><a href="{{ URL::to('statistik') }}"><span class="glyphicon glyphicon-equalizer"></span> statistik</a></li>
-		        @if(SessionLLhas('role'))
+		        @if(Session::has('role'))
 		        <li @yield('buat_pengaduan_active')><a href="{{ URL::to('buat-pengaduan') }}"><span class="glyphicon glyphicon-edit"></span> buat pengaduan</a></li>
 		        @endif
 		      </ul>
